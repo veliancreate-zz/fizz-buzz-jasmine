069ac44417c866782d831dd8723f6d78beb193a2
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-03 13:44:37 +0000

    finished testing

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,10 +1,40 @@
-describe('javaBuzz', function(){
+describe('javaBuzz', function() {
   var javaBuzz = new JavaBuzz
 
-  describe('knows when a number is', function(){
+  describe('knows when a number is', function() {
     
-    it('divisible by three', function(){
-      expect(javaBuzz.isDivisibleByThree(3)).toBe(true)       
+    it('divisible by three', function() {
+      expect(javaBuzz.isDivisibleByThree(3)).toBe(true);       
+    });
+
+    it('divisible by five', function() {
+      expect(javaBuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('not divisible by three', function() {
+      expect(javaBuzz.isDivisibleByThree(4)).toBe(false);       
+    });
+
+    it('divisible by five', function() {
+      expect(javaBuzz.isDivisibleByFive(2)).toBe(false);
+    });
+
+    it('is divisible by five and three', function() {
+      expect(javaBuzz.isDivisibleByFiveAndThree(43)).toBe(false);
+    });
+
+  });
+
+  describe('says', function(){
+
+    it('fizz when a number is divisible by three', function(){
+      expect(javaBuzz.say(3)).toEqual('Fizz');
+    });
+    it('buzz when a number is divisible by five', function(){
+      expect(javaBuzz.say(5)).toEqual('Buzz');
+    });
+    it('fizzbuzz when a number is divisible by five and three', function(){
+      expect(javaBuzz.say(15)).toEqual('Fizzbuzz')
     });
 
   });
--- src/javaBuzz.js
+++ src/javaBuzz.js
@@ -1,5 +1,28 @@
 JavaBuzz = function () {};
 
+
 JavaBuzz.prototype.isDivisibleByThree = function(number){
-  return true;
+  return(this._isDivisibleBy(3, number));
+};
+
+JavaBuzz.prototype.isDivisibleByFive = function(number){
+  return(this._isDivisibleBy(5, number));
+};
+
+JavaBuzz.prototype.isDivisibleByFiveAndThree = function(number){
+  return(this.isDivisibleByFive(number) && this.isDivisibleByThree(number));
+};
+
+JavaBuzz.prototype._isDivisibleBy = function(divisor, number){
+  return(number % divisor === 0);
+};
+
+JavaBuzz.prototype.say = function(number){
+  if(this.isDivisibleByFiveAndThree(number)){
+    return "Fizzbuzz";   
+  }else if(this.isDivisibleByFive(number)){ 
+    return "Buzz";
+  }else if(this.isDivisibleByThree(number)){
+    return "Fizz"; 
+  }; 
 };
